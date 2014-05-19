0f7da65d0ffbe603fc63717faf36c41305ab274c
Kryten2k35 <kryten2k35@gmail.com>
2014-05-19 22:54:31 +0100

    Fixed keyboard. My bad

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 del SGS_Kitkat_Test.zip
-cd out
-..\_tools\7za.exe a ..\SGS_Kitkat_Test.zip *
+xcopy /E out\* temp\
+xcopy /E /Y temp\overlay\system temp\system
+cd temp
+rmdir /S /Q temp\overlay
+..\_tools\7za.exe a -mx9 ..\SGS_Kitkat_Test.zip *
 cd ..
+rmdir /S /Q temp
