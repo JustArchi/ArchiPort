2832edb31f03506ddb30ed4bfcc11b8b53cf9ee4
peruginia <peruginia@tiscalinet.it>
2014-06-03 14:00:51 +0200

    Update build.bat
    bad 7zip options, incude of overlay "delete" previous file in zip. i think is the same on linux .sh

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 _tools\7za.exe u -up0q0r2x2y2z1w2 -mx9 ArchiPort_Test.zip ".\out\META-INF" ".\out\system" ".\out\boot.img"
 cd out/overlay
-..\..\_tools/7za u -up0q0r2x2y2z1w2 -mx9 ..\..\ArchiPort_Test.zip system
+..\..\_tools\7za u -up1q1r2x2y2z1w2 -mx9 ..\..\ArchiPort_Test.zip system
 cd ..\..\
 pause
